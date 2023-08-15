9f24c881da21b9445509373ef5bb8ba5f291e502
Max <mlmason11@gmail.com>
2023-08-15 11:38:36 -0400

    fixed Error Page file type pt 2

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom"
+
+function ErrorPage () {
+
+    const routeError = useRouteError()
+
+    return(
+        <div>
+            <h1>OH NOES!!!</h1>
+            <h3>{routeError.status}</h3>
+            <h3>{routeError.status}</h3>
+        </div>
+    
+    )
+}
+
+export default ErrorPage
